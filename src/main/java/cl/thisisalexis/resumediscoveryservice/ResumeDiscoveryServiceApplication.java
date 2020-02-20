7cb6f02c196134e903653822dd2eabc455c7744c
Alexis <alexis.ve@gmail.com>
2020-02-20 00:00:34 -0300

    chor: added spring boot library and all dependencies

---
+++ src/main/java/cl/thisisalexis/resumediscoveryservice/ResumeDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.thisisalexis.resumediscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeDiscoveryServiceApplication.class, args);
+	}
+
+}
